efcba9761305c2cb0fc9cf7b3801480c9e3f8e21
BigRockerEnding <bigrockerending@live.com>
2016-02-10 17:32:37 -0800

    Intro to Java - random shape

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,11 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-
-	
-		
-		
-	}
+Robot pauBot = new Robot ();
+for (int i = 0; i < 4; i++); {
+	pauBot.penDown();
+	pauBot.move(100);
+	pauBot.turn(90);
+}
+}
 }
--- src/day1/robot/Spiral.java
+++ src/day1/robot/Spiral.java
@@ -6,15 +6,27 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
+		Robot pauBot = new Robot ();
 		
 		// 5. Set your robot's pen to the down position
+		pauBot.penDown();
 
 		// 3. Set the robot to go at max speed (10)
+		pauBot.setSpeed(10);
 
 		// 4. Do the following (steps 7-9) 75 times
 
 			// 7. Change the pen color to random
+	for (int i = 0; i < 75; i++) {
+		pauBot.move(i*5);
+		pauBot.turn(360/7);
+		pauBot.setPenWidth(i);
+		pauBot.setRandomPenColor();
+		
+		
 	
+	
+	}
 			// 6. Move the robot 5 times the current line number you are drawing (5*i)
 	
 			// 2. Turn the robot 1/3 of 360 degrees to the right
